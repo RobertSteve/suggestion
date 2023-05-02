35f43b869f3f4220eb6f5bf38c549263c2e951bc
robertSteve <robert.paredes@outlook.com>
2023-05-02 20:03:49 +0000

    first commit

---
+++ src/Affinity.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace src;
+
+use GuzzleHttp\Client;
+
+class Affinity
+{
+
+    public function get($token, $workOfferId, $trsId)
+    {
+
+        $client = new Client();
+
+        $response = $client->post('https://21e58lr7ua.execute-api.us-east-1.amazonaws.com/prd/affinity-ml', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'x-api-key' => $token
+            ],
+            'json' => [
+                'work_offer_ids' => $workOfferId,
+                'trs_id' => $trsId
+            ]
+        ]);
+
+        return $response->getBody()->getContents();
+
+    }
+
+}
